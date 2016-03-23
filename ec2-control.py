72a6474454b097655d581f19d5a28c04b4720dc1
Doug Harris <dharris@gmail.com>
2016-03-23 13:54:04 -0400

    Cleaning up, main script ready to go

--- ec2-control.py
+++ ec2-control.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 #
-# Adapted from https://mcdee.com.au/nice-and-simple-aws-ec2-start-stop-script-python/
-#
  
 import sys
 import logging
@@ -23,24 +21,10 @@ boto.set_file_logger('boto', '/tmp/mc-control.log')
 try:
     from settings import *
 except ImportError:
-    logging.warning("""Settings file doesn't exist.\n
-    Copy settings.py.sample to settings.py and change its values""")
+    logging.warning("Settings file doesn't exist.\n"
+    "Copy settings.py.sample to settings.py and change its values")
     sys.exit(1)
 
-def print_usage(args):
-  print 'Usage:', args[0], 'stop|start <instance name>'
-  sys.exit(1)
- 
-def usage(args):
-  actions = ['stop', 'start']
-  if not len(args) == 3:
-    print_usage(args)
-  else:
-    if not args[1] in arg1:
-      print_usage(args)
-    else:
-      return args[2]
- 
 conn = boto.ec2.connect_to_region(REGION,
                                   aws_access_key_id=AWS_ACCESS_KEY_ID,
                                   aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
@@ -52,33 +36,31 @@ except EC2ResponseError, e:
                   % INSTANCE)
     sys.exit(1)
 
-print(inst.state)
-
-sys.exit(1)
- 
-
-if sys.argv[1] == 'start':
-  try:
-    inst = conn.get_all_instances(filters={'tag:Name': myinstance})[0].instances[0]
-  except IndexError:
-    print 'Error:', myinstance, 'not found!'
+logging.debug("instance status: %s" % inst.state)
+if inst.state not in ['running', 'stopped']:
+    logging.warning("Instance currently %s. Please run again when it has "
+                    "stopped.")
     sys.exit(1)
-  if not inst.state == 'running':
-    print 'Starting', myinstance
+
+action = 'toggle'
+try:
+    action = sys.argv[1]
+except IndexError:
+    pass
+
+if action in ['start', 'launch', 'up']:
+    action = 'start'
+elif action in ['stop', 'kill', 'down']:
+    action = 'stop'
+else:
+    action = 'toggle'
+
+logging.debug("action: %s" % action)
+
+if action == 'start' or (action == 'toggle' and inst.state != 'running'):
+    logging.info("Starting up...")
     inst.start()
-  else:
-    print 'Error:', myinstance, 'already running or starting up!'
-    sys.exit(1)
  
-if sys.argv[1] == 'stop':
-  try:
-    inst = conn.get_all_instances(filters={'tag:Name': myinstance})[0].instances[0]
-  except IndexError:
-    print 'Error:', myinstance, 'not found!'
-    sys.exit(1)
-  if inst.state == 'running':
-    print 'Stopping', myinstance
+if action == 'stop' or (action == 'toggle' and inst.state == 'running'):
+    logging.info("Stopping")
     inst.stop()
-  else:
-    print 'Error:', myinstance, 'already stopped or stopping'
-    sys.exit(1)
